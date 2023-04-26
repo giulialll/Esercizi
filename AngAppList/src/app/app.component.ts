de5d25779836615ac73c9e268f5becad9063246c
ndoronzo95 <nicola.doronzo@1nnovation.net>
2023-04-26 10:39:04 +0200

    cleanup

--- AngAppList/src/app/app.component.ts
+++ AngAppList/src/app/app.component.ts
@@ -4,31 +4,22 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation : ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-   allTasks: any[] = [];
+  allCompleted: any[] = [];
+  allTasks: any[] = [];
 
-aggiungiTask(task: string) {
-  this.allTasks.push(task);
+  aggiungiTask(task: string) {
+    this.allTasks.push(task);
+  }
 
-}
+  completaTask(index: number) {
+    this.allCompleted.push(this.allTasks[index]);
+    this.allTasks.splice(index, 1);
+  }
 
-allCompleted: any [] = [];
-completaTask(index : number){
-  this.allCompleted.push(this.allTasks[index]);
-  this.allTasks.splice(index,1)
-}
-
-rimuoviTask(index:number){
-  this.allCompleted.splice(index, 1);
-}
-
-
-
-
-
-
-
-
+  rimuoviTask(index: number) {
+    this.allCompleted.splice(index, 1);
+  }
 }
